3bf0603b33f4fb82fc5f3cbedcf1e06ca3938bc5
Thiago da Silva <thiago180244@sou.faccat.br>
2025-04-15 21:11:05 -0300

    Update exec8.c

--- exec8.c
+++ exec8.c
@@ -2,5 +2,21 @@
 #include <windows.h>
 
 int main() {
-    
+    system("cls");
+    int valor = 0;
+    int total = 0;
+    int numeros = 0;
+    printf("Digite seus valores (-1) para parar: ");
+    scanf("%d", &valor);
+
+    while (valor != -1) {
+        total += valor;
+        numeros++;
+        printf("Digite seus valores (digite -1 para parar): ");
+        scanf("%d", &valor);
+    }
+    printf("Voce digitou %d numeros \n", numeros);
+    printf("E o total foi %d \n", total);
+
+    return 0;
 }
